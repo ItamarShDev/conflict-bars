3bd523b40f4395b3725b77722934e05d49598910
Itamar Sharify <itamarsharify@gmail.com>
2025-09-19 23:08:47 +0300

    feat: update site metadata and add Peled to timeline artists

--- src/app/timeline/index.ts
+++ src/app/timeline/index.ts
@@ -3,10 +3,12 @@ import { shabakSamech } from './shabak-samech';
 import { sagol59 } from './sagol-59';
 import { tuna } from './tuna';
 import { TimelineEvent } from './types';
+import { peled } from './peled';
 
 export const timeline: TimelineEvent[] = [
     ...subliminal,
     ...shabakSamech,
     ...sagol59,
     ...tuna,
+    ...peled,
 ]
